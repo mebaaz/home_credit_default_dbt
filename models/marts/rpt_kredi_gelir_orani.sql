ac6866d46f50e3b32d22bd177c0b7d2216888642
barsmehmet@yahoo.com <barsmehmet@yahoo.com>
2026-08-21 06:25:24 +0000

    kredi_gelir_orani ismi düzeltildi

--- models/marts/rpt_kredi_gelir_orani.sql
+++ models/marts/rpt_kredi_gelir_orani.sql
@@ -17,7 +17,7 @@ select
         when kredi_gelir_orani < 3 then 'Gelirin_2_3_kati'
         when kredi_gelir_orani < 5 then 'Gelirin_3_5_kati'
         else                            'Gelirinin 5 Katindan fazla'
-    end                              as Kradi_Gelir_Oranı,
+    end                              as kredi_gelir_orani,
     count(*)                         as musteri_sayisi,
     round(100 * avg(TARGET), 2)      as temerrut_pct
 from ratio
