cd48b407f32116f5a5c1220031b4efe0b6848c64
barsmehmet@yahoo.com <barsmehmet@yahoo.com>
2026-08-21 06:08:40 +0000

    geririn katları isimlendirmeleri düzeltildi.

--- models/marts/rpt_kredi_gelir_orani.sql
+++ models/marts/rpt_kredi_gelir_orani.sql
@@ -12,11 +12,11 @@ with ratio as (
 
 select
     case
-        when kredi_gelir_orani < 1 then 'gelirin_altinda (<1x)'
-        when kredi_gelir_orani < 2 then 'gelirin_1_2_kati'
-        when kredi_gelir_orani < 3 then 'gelirin_2_3_kati'
-        when kredi_gelir_orani < 5 then 'gelirin_3_5_kati'
-        else                            'gelirin_5_kati_ustu'
+        when kredi_gelir_orani < 1 then 'Gelirin altinda <1x'
+        when kredi_gelir_orani < 2 then 'Gelirin_1_2_kati'
+        when kredi_gelir_orani < 3 then 'Gelirin_2_3_kati'
+        when kredi_gelir_orani < 5 then 'Gelirin_3_5_kati'
+        else                            'Gelirinin 5 Katindan fazla'
     end                              as oran_grubu,
     count(*)                         as musteri_sayisi,
     round(100 * avg(TARGET), 2)      as temerrut_pct
